1a1ee5e0f1b901f45c673c12f6da4a6964df7c5f
Abhinav <abhinav.zimmy@gmail.com>
2022-07-31 20:44:47 +0530

    minor change

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 const bodyParser = require("body-parser");
 
 const apiRoute = require("./routes/api-route");
